22da945e6c921d1fed516e5ef318e278aa571578
CarterCook12345 <89489627+CarterCook12345@users.noreply.github.com>
2021-08-30 12:05:33 -0700

    Activity 2 done

--- Crab.java
+++ Crab.java
@@ -7,8 +7,28 @@ public class Crab extends Actor
 {
     public void act()
     {
-        // Add your action code here
+        move(3);
+        turnAtEdge();
+        checkKeyPress();
+        onCollision();
+    }
+    //Turns the Crab at the edge
+    private void turnAtEdge()
+    {
+        if (isAtEdge())
+        {
+            turn(50);
+        }
+    }
+    //Checks for user key prsses so user can turn the Crab
+    private void checkKeyPress()
+    {
+        
+    }
+    //Checks for collisions with other objects
+    private void onCollision()
+    {
+        
     }
 }
-
 
